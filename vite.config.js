2f099ba13596c127499ec177ec9945b85a4de117
RichardModin <richard.modin@gmail.com>
2025-05-12 15:00:43 -0400

    fixing deployment again

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import { version } from './package.json';
 import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/songs-for-tabla/',
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   define: {
     __APP_VERSION__: JSON.stringify(version),
   },
